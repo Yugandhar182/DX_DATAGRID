f1b6f5a8ba75cb9f6008657f5a009b66d4637f35
Yugandhar182 <yugandhar@recruitly.io>
2023-06-10 18:01:12 +0530

    .

--- src/App.svelte
+++ src/App.svelte
@@ -123,7 +123,7 @@
 		},
 		onRowRemoving: async (e) => {
 		  try {
-			const response = await fetch(`https://api.recruitly.io/api/candidate/?apiKey:TEST1236C4CF23E6921C41429A6E1D546AC9535E`, {
+			const response = await fetch(`https://api.recruitly.io/api/candidate/?apiKey=TEST1236C4CF23E6921C41429A6E1D546AC9535E`, {
 			  method: "DELETE",
 			  
 			});
